5c8c505b2816375c9f8b4d0dfd3f0be593539d1d
Nomjeen729 <nomionini0729@gmail.com>
2025-09-23 00:39:49 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,23 @@ import "./globals.css";
 export const metadata = {
   title: "Happy Birthday Mr.J!",
   description: "A celebration of our love",
-}
+  // Google verification meta-г энд оруулж болно
+  verification: {
+    google: "F8zqgEe9L90EtXC2h7iQuCHOUO97ADKa8ACQNIb0TfQ",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`antialiased`}
-      >
+      <head>
+        {/* Google Search Console verification */}
+        <meta
+          name="google-site-verification"
+          content="F8zqgEe9L90EtXC2h7iQuCHOUO97ADKa8ACQNIb0TfQ"
+        />
+      </head>
+      <body className="antialiased">
         {children}
       </body>
     </html>
